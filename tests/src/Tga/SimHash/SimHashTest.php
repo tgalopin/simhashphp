42eaed7f5ab8440bc6e4bb13662e43ec478bc8fe
Titouan Galopin <galopintitouan@gmail.com>
2015-05-31 23:13:40 +0200

    Add licence on some files

--- tests/src/Tga/SimHash/SimHashTest.php
+++ tests/src/Tga/SimHash/SimHashTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SimHashPhp package.
+ *
+ * (c) Titouan Galopin <http://titouangalopin.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Tga\SimHash;
 
 use Tga\SimHash\Comparator\GaussianComparator;
